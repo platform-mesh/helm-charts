613951eeb85bdb050f4c3719a10bfe8a08cabd4d
Bastian Echterhölter <bastian.echterhoelter@sap.com>
2025-10-06 10:46:47 +0200

    feat: local setup with org auth support (#152)

--- local-setup/scripts/createKcpAdminKubeconfig.sh
+++ local-setup/scripts/createKcpAdminKubeconfig.sh
@@ -6,7 +6,7 @@ KCP_URL=https://kcp.api.portal.dev.local:8443
 #KCP_URL=https://kcp.api.portal.cc-one.showroom.apeirora.eu
 
 mkdir -p $PWD/.secret/kcp
-kubectl get secret $KCP_CA_SECRET -n platform-mesh-system -o=jsonpath='{.data.tls\.crt}' | base64 -d > $PWD/.secret/kcp/ca.crt
+kubectl get secret $KCP_CA_SECRET -n platform-mesh-system -o=jsonpath='{.data.ca\.crt}' | base64 -d > $PWD/.secret/kcp/ca.crt
 kubectl --kubeconfig=$PWD/.secret/kcp/admin.kubeconfig config set-cluster --embed-certs  workspace.kcp.io/current --server $KCP_URL/clusters/root --certificate-authority=$PWD/.secret/kcp/ca.crt
 kubectl get secret $KCP_ADMIN_SECRET -n platform-mesh-system -o=jsonpath='{.data.tls\.crt}' | base64 -d > $PWD/.secret/kcp/client.crt
 kubectl get secret $KCP_ADMIN_SECRET -n platform-mesh-system -o=jsonpath='{.data.tls\.key}' | base64 -d > $PWD/.secret/kcp/client.key
