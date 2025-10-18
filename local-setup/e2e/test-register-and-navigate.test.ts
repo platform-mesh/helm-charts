43a8d7f827ae0ae683a688a10a9d76bb8c083975
Angel Kafazov <akafazov@cst-bg.net>
2025-10-18 15:48:42 +0300

    feat: add feature toggle for getting started in platform mesh configuration and update e2e tests
    On-behalf-of: @SAP angel.kafazov@sap.com
    Signed-off-by: Angel Kafazov <akafazov@cst-bg.net>

--- local-setup/e2e/test-register-and-navigate.test.ts
+++ local-setup/e2e/test-register-and-navigate.test.ts
@@ -1,6 +1,7 @@
 import test, { expect, Page } from '@playwright/test';
 
 const portalBaseUrl = 'https://portal.dev.local:8443/';
+const testaccountName = 'testaccount1';
 
 async function activateUserEmailViaMailpit(
   page: Page,
@@ -45,6 +46,55 @@ test.describe('Home Page', () => {
     await newPage.waitForLoadState('domcontentloaded');
     const verificationText = await newPage.getByText("Welcome to the Platform Mesh Portal!");
     await expect(verificationText).toBeVisible();
+
+    await newPage.getByRole('button', { name: 'Switch' }).click();
+
+    const loginText = await newPage.getByText("Sign in to your account");
+    await expect(loginText).toBeVisible();
+
+    await newPage.screenshot({ path: 'post-login2.png' });
+
+    // Perform register
+    await newPage.getByText('Register').click();
+
+    await newPage.fill('input[id="email"]', userEmail);
+    await newPage.fill('input[id="password"]', userPassword);
+    await newPage.fill('input[id="password-confirm"]', userPassword);
+    await newPage.fill('input[id="firstName"]', firstName);
+    await newPage.fill('input[id="lastName"]', lastName);
+
+    await newPage.locator('input[value="Register"]').click();
+    const newPage2 = await activateUserEmailViaMailpit(newPage, userEmail);
+    const welcomeText = await newPage2.getByText("Welcome! Let's get started.");
+    await expect(welcomeText).toBeVisible();
+    await newPage2.getByText('Accounts').click();
+    
+    // click on "Create" button
+    await newPage2.getByRole('button', { name: 'Create' }).click();
+
+    // await newPage2.pause();
+
+    await newPage2.locator('.ui5-select-icon > .ui5-icon-root').click();
+    await newPage2.locator('#ui5wc_10-content > .ui5-li-text-wrapper').click();
+    await newPage2.locator('#inner').nth(1).click();
+    await newPage2.locator('ui5-input').filter({ hasText: '<svg xmlns="http://www.w3.org' }).locator('#inner').click();
+    await newPage2.locator('ui5-input').filter({ hasText: '<svg xmlns="http://www.w3.org' }).locator('#inner').click();
+    await newPage2.locator('ui5-input').filter({ hasText: '<svg xmlns="http://www.w3.org' }).locator('#inner').press('Shift+Home');
+    await newPage2.locator('ui5-input').filter({ hasText: '<svg xmlns="http://www.w3.org' }).locator('#inner').fill(testaccountName);
+    await newPage2.getByRole('button', { name: 'Submit Emphasized' }).click();
+
+    
+    await newPage2.getByText(testaccountName).click();
+    const download1Promise = newPage2.waitForEvent('download');
+    await newPage2.getByRole('button', { name: 'Download kubeconfig Emphasized' }).click();
+    const download1 = await download1Promise;
+    await newPage2.getByTestId('luigi-topnav-title').click();
+    await newPage2.getByTestId('accounts_accounts').click();
+    
+    await newPage2.pause();
+
+    const accountText = await newPage2.getByText(testaccountName);
+    await expect(accountText).toBeVisible();
 
   });
 });
