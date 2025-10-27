ca05c0cb62a2027cdaf28c7d49e72e4f235058c6
Bastian Echterhölter <bastian.echterhoelter@sap.com>
2025-10-27 09:44:59 +0100

    fix: update component version to 0.1.0-rc.328 and clean up start script output
    Signed-off-by: Bastian Echterhölter <bastian.echterhoelter@sap.com>
    On-behalf-of: @SAP <bastian.echterhoelter@sap.com>

--- local-setup/scripts/start.sh
+++ local-setup/scripts/start.sh
@@ -173,14 +173,6 @@ fi
 echo -e "${COL}[$(date '+%H:%M:%S')] Preparing KCP Secrets for admin access ${COL_RES}"
 $SCRIPT_DIR/createKcpAdminKubeconfig.sh
 
-echo -e "${COL}[$(date '+%H:%M:%S')] Waiting for default organization to be ready ${COL_RES}"
-
-kubectl wait \
-  --server='https://kcp.api.portal.dev.local:8443/clusters/root:orgs' \
-  --kubeconfig=$SCRIPT_DIR/../../.secret/kcp/admin.kubeconfig \
-  --for=condition=Ready accounts \
-  --timeout=280s default
-
 echo -e "${COL}Please create an entry in your /etc/hosts with the following line: \"127.0.0.1 default.portal.dev.local portal.dev.local kcp.api.portal.dev.local\" ${COL_RES}"
 show_wsl_hosts_guidance
 
@@ -193,7 +185,7 @@ echo -e "${COL}Once kcp is up and running, run '\033[0;32mexport KUBECONFIG=$(pw
 echo -e "${COL}-------------------------------------${COL_RES}"
 echo -e "${COL}[$(date '+%H:%M:%S')] Installation Complete ${RED}♥${COL} !${COL_RES}"
 echo -e "${COL}-------------------------------------${COL_RES}"
-echo -e "${COL}You can access the onboarding portal at: https://portal.dev.local:8443 or the default organization at: https://default.portal.dev.local:8443 ${COL_RES}"
+echo -e "${COL}You can access the onboarding portal at: https://portal.dev.local:8443 ${COL_RES}"
 
 
 if ! git diff --quiet $SCRIPT_DIR/../kustomize/components/platform-mesh-operator-resource/platform-mesh.yaml; then
