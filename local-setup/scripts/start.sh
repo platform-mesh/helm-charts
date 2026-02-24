dc7fcfc82208544dc0a2880a10f4353c0888fc06
Oleg Ershov <97069709+OlegErshov@users.noreply.github.com>
2026-02-24 19:13:31 +0300

    chore: ocm version upgrade (#1160)
    * chore: use helmRepository and helmRelease instead of kustomise patches
    
    On-behalf-of: SAP aleh.yarshou@sap.com
    
    * fix: helmrepository reference in prerelese
    
    On-behalf-of: SAP aleh.yarshou@sap.com
    
    * use patch for ocm-k8s-tooklit in prerelease overlay
    
    On-behalf-of: SAP aleh.yarshou@sap.com
    
    * feat: add renovate config for ocm toolkit update
    
    On-behalf-of: SAP aleh.yarshou@sap.com

--- local-setup/scripts/start.sh
+++ local-setup/scripts/start.sh
@@ -133,6 +133,10 @@ kubectl wait --namespace default \
   --for=condition=Ready helmreleases \
   --timeout=$KUBECTL_WAIT_TIMEOUT kro
 
+kubectl wait --namespace default \
+  --for=condition=Ready helmreleases \
+  --timeout=$KUBECTL_WAIT_TIMEOUT ocm-k8s-toolkit
+
 echo -e "${COL}[$(date '+%H:%M:%S')] Creating necessary secrets ${COL_RES}"
 #kubectl create secret tls iam-authorization-webhook-webhook-ca -n platform-mesh-system --key $SCRIPT_DIR/../webhook-config/ca.key --cert $SCRIPT_DIR/../webhook-config/ca.crt --dry-run=client -o yaml | kubectl apply -f -
 kubectl create secret generic keycloak-admin -n platform-mesh-system --from-literal=secret=admin --dry-run=client -o yaml | kubectl apply -f -
