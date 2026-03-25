4b9c9d57045a2cb0e4c93d379bc35dad7761a908
Angel Kafazov <akafazov@cst-bg.net>
2026-03-25 16:16:20 +0200

    minor fixes
    On-behalf-of: @SAP angel.kafazov@sap.com
    Signed-off-by: Angel Kafazov <akafazov@cst-bg.net>

--- local-setup/e2e/helpers/runtime.ts
+++ local-setup/e2e/helpers/runtime.ts
@@ -1,6 +1,7 @@
 import { execFileSync } from 'node:child_process';
+import { existsSync } from 'node:fs';
 
-import { adminKubeconfigPath } from './constants';
+import { adminKubeconfigPath, runtimeKubeconfigPath, infraKubeconfigPath } from './constants';
 
 function runKubectlWithKubeconfig(kubeconfigPath: string, args: string[], input?: string): string {
   return execFileSync('kubectl', ['--kubeconfig', kubeconfigPath, ...args], {
@@ -21,10 +22,24 @@ function runAdminKubectl(args: string[], input?: string): string {
 }
 
 function runRuntimeKubectl(args: string[], input?: string): string {
+  // In remote mode, the runtime cluster has its own kubeconfig
+  if (existsSync(runtimeKubeconfigPath)) {
+    return execFileSync('kubectl', ['--kubeconfig', runtimeKubeconfigPath, ...args], {
+      encoding: 'utf8',
+      input,
+    }).trim();
+  }
   return execFileSync('kubectl', args, {
     encoding: 'utf8',
     input,
   }).trim();
 }
 
-export { runKubectlWithKubeconfig, runAdminKubectl, runRuntimeKubectl };
+function runInfraKubectl(args: string[], input?: string): string {
+  return execFileSync('kubectl', ['--kubeconfig', infraKubeconfigPath, ...args], {
+    encoding: 'utf8',
+    input,
+  }).trim();
+}
+
+export { runKubectlWithKubeconfig, runAdminKubectl, runRuntimeKubectl, runInfraKubectl };
