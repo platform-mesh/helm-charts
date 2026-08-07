94364bf0acf92f2bf00b24aa706dca121a65e60b
Angel Kafazov <akafazov@cst-bg.net>
2026-08-07 14:57:16 +0300

    chore: remove keycloak component and OCM CLI binary setup from prerelease script
    Signed-off-by: Angel Kafazov <akafazov@cst-bg.net>

--- local-setup/scripts/setup-prerelease.sh
+++ local-setup/scripts/setup-prerelease.sh
@@ -53,11 +53,6 @@ deploy_transfer_pod() {
   kubectl wait --namespace default --for=condition=Ready pod --timeout=480s ocm-transfer-pod
   kubectl exec $(get_kubectl_exec_flags) ocm-transfer-pod -- mkdir -p .ocm
 
-  # Copy the host OCM CLI binary into the pod so it runs v2 regardless of what the image ships
-  local host_ocm="${LOCAL_BIN:-$(cd "$SCRIPT_DIR/../.." && pwd)/bin}/ocm"
-  cat "$host_ocm" | kubectl exec -i ocm-transfer-pod -- tee /tmp/ocm > /dev/null
-  kubectl exec $(get_kubectl_exec_flags) ocm-transfer-pod -- sudo install -m 0755 /tmp/ocm /usr/local/bin/ocm
-
   # Configure CA on the pod
   kubectl exec $(get_kubectl_exec_flags) ocm-transfer-pod -- openssl s_client -connect oci-registry-docker-registry.registry.svc.cluster.local:443 -showcerts </dev/null 2>/dev/null| openssl x509 -outform PEM > $SCRIPT_DIR/registry-ca.pem
   kubectl cp $SCRIPT_DIR/registry-ca.pem -n default ocm-transfer-pod:registry-ca.pem
